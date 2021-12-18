84c9fd8cca6307f76195afb4b34ae2660493945b
thuvan2512 <thunguyenvan2512@gmail.com>
2021-12-18 11:10:59 +0700

    add alo

--- clinicapp/__init__.py
+++ clinicapp/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.secret_key = '@pdot324g942##$%$^$@%#%^%$^*&*(&)(3jifgj34-94'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Huy12345678@localhost/clinicapp?charset=utf8mb4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:25122k@localhost/clinicapp?charset=utf8mb4'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 login = LoginManager(app=app)
 
